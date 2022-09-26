0b3139ca7c94c937f3819f727384657dcd6f909f
Katharina Bäuml <katharina.baeuml@immowelt.de>
2022-09-26 14:28:31 +0200

    Jan hilft mir

--- fortodo.js
+++ fortodo.js
@@ -18,7 +18,7 @@ var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-        var div = this.parentElement;
+        var  div = this.parentElement;
         div.style.display = "none";
         sessionStorage.removeItem(div)
     };console.log(div)
@@ -41,13 +41,17 @@ for (i = 0; i < close.length; i++) {
 function newElement() {
     var li = document.createElement("li"); // neues Element in der Liste erzeugen heißt jetzt var li
     var inputValue = document.getElementById("Eingabefeld").value; // Eingegebenes im Eingabefeld heißt jtzt inputValue
+    let incr = incr++;
+    console.log(incr)
     console.log(inputValue);
+
     var t = document.createTextNode(inputValue); // Eingegbenes im Textfeld wird neuer Text
     li.appendChild(t); // neuer Text(eingegeben im Einhabefeld) wird in vorhandene Liste aufgenommen
     if (inputValue === '') {
         alert("no input"); // falls nicht eingegeben wurde, erscheint Pop-Up mit text "no input"
     } else {
-        document.getElementById("myUl").appendChild(li); // wurde etwas eingegeben, bekommt die Liste "myUL" ein neues <li> mit dem Inhalt des Eingabefeldes
+        document.getElementById("myUl").appendChild(li)
+        // wurde etwas eingegeben, bekommt die Liste "myUL" ein neues <li> mit dem Inhalt des Eingabefeldes
     }
     document.getElementById("Eingabefeld").value = ""; // Das Eingabefeld wird wieder leer gemacht für neue Eingabe
 
@@ -60,13 +64,16 @@ sessionStorage.setItem(inputValue,"something else" )
     li.appendChild(span); // das li bekommt den neuen Inhalt span <li><span class = "close"></span></li>?
 
     for (i = 0; i < close.length; i++) { //geht alle Elemente von  span "close" durch
+        console.error(i);
         close[i].onclick = function () { // wird 
+            console.log(i)
             var div = this.parentNode; // auch .parentNode verwendbar
             div.style.display = "none"; // hide the div, also das parentElement
+           // sessionStorage.removeItem("xc")
         };
     }
 }
-
+//TODO: add undo delte function
 
 
 // sessionStorage.setItem("TempSaveStorage","wow")
